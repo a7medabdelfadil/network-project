6cfb5f7f4ac451ea5e169f63fcf92bc430c21bb6
Ahmed Abdelfadeel <a7med.abdelfadeel@gmail.com>
2025-12-07 20:42:05 +0200

    last inshaallah

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,9 @@
 const track = document.querySelector(".sdetails-slider__track");
 const dots = document.querySelectorAll(".sdetails-slider__dots .dot");
 
-dots.forEach((dot, index) => {
+dots.forEach((dot, index) => { // 0 1 2
   dot.addEventListener("click", () => {
-    track.style.transform = `translateX(-${index * 100}%)`; 
+    track.style.transform = `translateX(-${index * 100}%)`;
     dots.forEach((d) => d.classList.remove("active"));
     dot.classList.add("active");
   });
